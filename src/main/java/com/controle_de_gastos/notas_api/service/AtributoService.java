66bfc4f4e7638d38933e31cdd87f9014006c5205
Julioasec <ed_julio@outlook.com>
2025-09-26 15:27:12 -0300

    Criando o service de atributo AtributoService

---
+++ src/main/java/com/controle_de_gastos/notas_api/service/AtributoService.java
@@ -0,0 +1,22 @@
+package com.controle_de_gastos.notas_api.service;
+
+import com.controle_de_gastos.notas_api.model.Atributo;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class AtributoService {
+    private List<Atributo> atributos = new ArrayList<>();
+
+    public List<Atributo> ListarAtributos(){
+        return this.atributos;
+    }
+
+    public Atributo criarAtributo(Atributo atributo){
+        atributo.setIdAtributo((long) atributos.size()+1);
+        atributos.add(atributo);
+        return atributo;
+    }
+}
