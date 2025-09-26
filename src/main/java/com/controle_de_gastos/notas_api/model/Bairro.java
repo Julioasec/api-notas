776f8c9f2b7ee1ab898a8bc336a7507c6f3690bb
Julioasec <ed_julio@outlook.com>
2025-09-26 15:49:59 -0300

    criação do model Bairro

---
+++ src/main/java/com/controle_de_gastos/notas_api/model/Bairro.java
@@ -0,0 +1,17 @@
+package com.controle_de_gastos.notas_api.model;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Data
+@AllArgsConstructor
+public class Bairro {
+    private long id_bairro;
+    private String nome;
+
+}
