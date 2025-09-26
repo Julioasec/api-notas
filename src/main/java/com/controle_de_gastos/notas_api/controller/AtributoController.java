7c731f9e16f3d13602c2f8d1b13b33ecc36de828
Julioasec <ed_julio@outlook.com>
2025-09-26 18:14:08 -0300

    Criação do AtributoRepository e alteração no service e controller

--- src/main/java/com/controle_de_gastos/notas_api/controller/AtributoController.java
+++ src/main/java/com/controle_de_gastos/notas_api/controller/AtributoController.java
@@ -1,27 +1,48 @@
 package com.controle_de_gastos.notas_api.controller;
 
+import com.controle_de_gastos.notas_api.Repository.AtributoRepository;
 import com.controle_de_gastos.notas_api.model.Atributo;
 import com.controle_de_gastos.notas_api.service.AtributoService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/atributos")
 public class AtributoController {
 
+
+    private final AtributoService atributoService;
+    private final AtributoRepository atributoRepository;
+
     @Autowired
-    private AtributoService atributoService;
+    public AtributoController(AtributoService atributoService, AtributoRepository atributoRepository) {
+        this.atributoService = atributoService;
+        this.atributoRepository = atributoRepository;
+    }
+
 
     @GetMapping
     public List<Atributo> listarAtributos(){
-        return atributoService.listarAtributos();
+        return atributoService.listarTodos();
     }
 
+    @GetMapping("/{id}")
+    public Optional<Atributo> buscarPorId(@PathVariable Long id){
+        return atributoService.buscarPorId(id);
+    }
+
+
     @PostMapping
-    public Atributo criarAtributo(@RequestBody Atributo atributo){
-        return atributoService.criarAtributo(atributo);
+    public Atributo atributoSalvar(@RequestBody Atributo atributo){
+        return atributoService.salvar(atributo);
+    }
+
+    @DeleteMapping
+    public void deletarAtributo(Long id){
+         atributoService.deletarPorId(id);
     }
 
 }
--- src/main/java/com/controle_de_gastos/notas_api/service/AtributoService.java
+++ src/main/java/com/controle_de_gastos/notas_api/service/AtributoService.java
@@ -1,22 +1,34 @@
 package com.controle_de_gastos.notas_api.service;
 
+import com.controle_de_gastos.notas_api.Repository.AtributoRepository;
 import com.controle_de_gastos.notas_api.model.Atributo;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class AtributoService {
-    private List<Atributo> atributos = new ArrayList<>();
+    private final AtributoRepository atributoRepository;
 
-    public List<Atributo> listarAtributos(){
-        return this.atributos;
+    public AtributoService(AtributoRepository atributoRepository) {
+        this.atributoRepository = atributoRepository;
     }
 
-    public Atributo criarAtributo(Atributo atributo){
-        atributo.setIdAtributo((long) atributos.size()+1);
-        atributos.add(atributo);
-        return atributo;
+    public List<Atributo> listarTodos() {
+        return atributoRepository.findAll();
+    }
+
+    public Optional<Atributo> buscarPorId(Long id) {
+        return atributoRepository.findById(id);
+    }
+
+    public Atributo salvar(Atributo atributo){
+        return atributoRepository.save(atributo);
+    }
+
+    public void deletarPorId(Long id){
+        atributoRepository.deleteById(id);
     }
 }
