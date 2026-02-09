3b94a08f67db98563c68cae761f85560d2fff959
Julioasec <ed_julio@outlook.com>
2026-02-09 16:22:42 -0300

    refator(ItemAtributo): Implementar ResponseEntity nos endpoints

--- src/main/java/com/controle_de_gastos/notas_api/service/ItemAtributoJuncaoService.java
+++ src/main/java/com/controle_de_gastos/notas_api/service/ItemAtributoJuncaoService.java
@@ -64,8 +64,14 @@ public class ItemAtributoJuncaoService {
                 .map(this::toRespostaDTO);
     }
 
-    public void deletePorId(Integer idIAJuncao){
-        itemAtributoJuncaoRepository.deleteById(idIAJuncao);
-    }
+//    public boolean deletePorId(Integer id){
+//        ItemAtributoJuncao itemAtributoJuncao =  itemAtributoJuncaoRepository.findById(id).orElse(null);
+//
+//        if(itemAtributoJuncao == null) return false;
+//
+//        itemAtributoJuncaoRepository.delete(itemAtributoJuncao);
+//        return true;
+//    }
+
 
 }
