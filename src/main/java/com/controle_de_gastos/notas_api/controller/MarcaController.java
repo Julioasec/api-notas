527d4f54b4d70e1cabb2c40a163ef2a2903b6deb
Julioasec <ed_julio@outlook.com>
2026-02-19 00:40:43 -0300

    refactor(Marca): Adicionar ResponseEntity nos endpoints

--- src/main/java/com/controle_de_gastos/notas_api/controller/MarcaController.java
+++ src/main/java/com/controle_de_gastos/notas_api/controller/MarcaController.java
@@ -4,6 +4,7 @@ import com.controle_de_gastos.notas_api.dto.requisicao.MarcaRequisicaoDTO;
 import com.controle_de_gastos.notas_api.dto.resposta.MarcaRespostaDTO;
 import com.controle_de_gastos.notas_api.service.MarcaService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import java.util.List;
 import java.util.Optional;
@@ -16,18 +17,20 @@ public class MarcaController {
     private final MarcaService marcaService;
 
     @GetMapping
-    public List<MarcaRespostaDTO> listarMarcas(){
-        return marcaService.listarTodos();
+    public ResponseEntity<List<MarcaRespostaDTO>> listarMarcas(){
+        return ResponseEntity.ok(marcaService.listarTodos());
     }
 
     @GetMapping("/{id}")
-    public Optional<MarcaRespostaDTO> buscarMarcaPorId(@PathVariable Integer id){
-        return marcaService.buscarPorId(id);
+    public ResponseEntity<MarcaRespostaDTO> buscarMarcaPorId(@PathVariable Integer id){
+        return marcaService.buscarPorId(id)
+                .map(ResponseEntity::ok)
+                .orElseGet(()->ResponseEntity.notFound().build());
     }
 
     @PostMapping
-    public MarcaRespostaDTO criar(@RequestBody MarcaRequisicaoDTO marcaDTO) {
-        return marcaService.criar(marcaDTO);
+    public ResponseEntity<MarcaRespostaDTO> criar(@RequestBody MarcaRequisicaoDTO marcaDTO) {
+        return ResponseEntity.status(201).body(marcaService.criar(marcaDTO));
     }
 
     @DeleteMapping
