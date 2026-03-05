258151e4fec126c83e45d80a28dc436c1435e37a
Julioasec <ed_julio@outlook.com>
2026-03-05 17:19:18 -0300

    Implementar ResṕonseEntity nos endpoints

--- src/main/java/com/controle_de_gastos/notas_api/controller/ParcelamentoController.java
+++ src/main/java/com/controle_de_gastos/notas_api/controller/ParcelamentoController.java
@@ -4,6 +4,7 @@ import com.controle_de_gastos.notas_api.dto.resposta.ParcelamentoRespostaDTO;
 import com.controle_de_gastos.notas_api.service.ParcelamentoService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import java.util.List;
 import java.util.Optional;
@@ -16,19 +17,17 @@ public class ParcelamentoController {
         private final ParcelamentoService parcelamentoService;
 
         @GetMapping
-        public List<ParcelamentoRespostaDTO> listarTodos(){
-            return parcelamentoService.listarTodos();
+        public ResponseEntity<List<ParcelamentoRespostaDTO>> listarTodos(){
+
+           return ResponseEntity.ok(parcelamentoService.listarTodos());
         }
 
         @GetMapping("/{id}")
-        public Optional<ParcelamentoRespostaDTO> listarPorId(@PathVariable Integer id){
-            return parcelamentoService.buscarPorId(id);
+        public ResponseEntity<ParcelamentoRespostaDTO> listarPorId(@PathVariable Integer id){
+            return parcelamentoService.buscarPorId(id)
+                    .map(ResponseEntity::ok)
+                    .orElse(ResponseEntity.notFound().build());
         }
 
         //post está no NotaController
-
-        @DeleteMapping
-        public void deletarPorId(Integer id){
-            parcelamentoService.deletarPorId(id);
-        }
 }
